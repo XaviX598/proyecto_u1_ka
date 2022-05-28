9e866c5af7e3eb8c66ce443d528927ab1bd24856
Xavier Aguilar <kevinjkevps4@gmail.com>
2022-05-27 20:18:28 -0500

    Segundo cambio

--- src/main/java/com/uce/edu/demo/consultorio/Paciente.java
+++ src/main/java/com/uce/edu/demo/consultorio/Paciente.java
@@ -1,5 +1,24 @@
 package com.uce.edu.demo.consultorio;
 
 public class Paciente {
+	private String nombre;
+	private int edad;
+
+	// set y get
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 
 }
